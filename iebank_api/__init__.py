904452c0402fbdd60b65a07b16ff27d28efc5c07
Farah Orfaly <farah.orfaly@gmail.com>
2024-11-05 12:18:35 +0100

    Initialize release-1

---
+++ iebank_api/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+
+# Select environment based on the ENV environment variable
+if os.getenv('ENV') == 'local':
+    print("Running in local mode")
+    app.config.from_object('config.LocalConfig')
+elif os.getenv('ENV') == 'dev':
+    print("Running in development mode")
+    app.config.from_object('config.DevelopmentConfig')
+elif os.getenv('ENV') == 'ghci':
+    print("Running in github mode")
+    app.config.from_object('config.GithubCIConfig')
+elif os.getenv('ENV') == 'uat':
+    print("Running in github mode")
+    app.config.from_object('config.UATConfig')
+
+db = SQLAlchemy(app)
+
+from iebank_api.models import Account
+
+with app.app_context():
+    db.create_all()
+CORS(app)
+
+from iebank_api import routes
